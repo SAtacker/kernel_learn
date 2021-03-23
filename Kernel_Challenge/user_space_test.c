2f1ece571bd70105dcfcb99a123b4e42788c648c
Shreyas <shreyasatre16@gmail.com>
2021-03-24 01:36:46 +0530

    Refactored code and made output look better

--- Kernel_Challenge/user_space_test.c
+++ Kernel_Challenge/user_space_test.c
@@ -14,63 +14,67 @@
 #include <unistd.h>
 #include <sys/ioctl.h>
 
-#define WR_VALUE _IOW('a',1,char*)
-#define RD_VALUE _IOR('a',2,char*)
-#define BUFFER_LENGTH 256 
+#define WR_VALUE _IOW('a', 1, char *)
+#define RD_VALUE _IOR('a', 2, char *)
+#define BUFFER_LENGTH 256
+
 static char _rec_buf[BUFFER_LENGTH];
- 
+
 int main()
 {
     int ret, fd;
     char _send_buf[BUFFER_LENGTH];
-    
+
     printf("========== User Space Testing ==========\n");
-    
+
     fd = open("/dev/char_ddriver", O_RDWR);
-    
+
     if (fd < 0)
     {
         perror("Error opening");
         return errno;
     }
-    
+
     printf("Send Buffer:\n");
-    
+
     scanf("%[^\n]%*c", _send_buf);
     printf("Writing to the device [%s].\n", _send_buf);
-    
+
     ret = write(fd, _send_buf, strlen(_send_buf));
     if (ret < 0)
     {
         perror("Error Writing");
         return errno;
     }
-    
+
     printf("ENTER to read\n");
     getchar();
     printf("Reading\n");
-    
-    ret = read(fd, _rec_buf, BUFFER_LENGTH); 
-    if (ret < 0){
+
+    ret = read(fd, _rec_buf, BUFFER_LENGTH);
+    if (ret < 0)
+    {
         perror("Failed to read");
         return errno;
     }
-    
+
     printf("Message from LKM [%s]\n", _rec_buf);
 
-    memset(_rec_buf,'\0',sizeof(_rec_buf));
-    memset(_send_buf,'\0',sizeof(_send_buf));
-    
-    printf("========== IOCTL ==========\n");
+    memset(_rec_buf, '\0', sizeof(_rec_buf));
+    memset(_send_buf, '\0', sizeof(_send_buf));
+
+    printf("=============== IOCTL ==================\n");
 
     printf("Enter the Value to send\n");
-    scanf("%[^\n]%*c",_send_buf);
+    scanf("%[^\n]%*c", _send_buf);
     printf("Writing Value to Driver\n");
-    ioctl(fd, WR_VALUE, (char*)_send_buf); 
+    ioctl(fd, WR_VALUE, (char *)_send_buf);
 
-    printf("Reading Value from Driver\n");
-    ioctl(fd, RD_VALUE, (char*)_rec_buf);
+    printf("ENTER to read\n");
+    getchar();
+    printf("Reading\n");
+    ioctl(fd, RD_VALUE, (char *)_rec_buf);
     printf("Value is %s\n", _rec_buf);
-    
-   return 0;
+
+    return 0;
 }
